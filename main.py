4f42bde5d8260dba34b4c6ce92dbe7bad6baff5a
argadepp <argadepp@gmail.com>
2025-07-17 17:56:30 +0530

    testting dagger

---
+++ main.py
@@ -0,0 +1,18 @@
+import dagger
+
+async def main():
+    async with dagger.Connection() as client:
+        src = client.host().directory(".")
+
+        container = (
+            client.container()
+            .from_("python:3.11")
+            .with_mounted_directory("/app", src)
+            .with_workdir("/app")
+            .with_exec(["pip", "install", "-r", "requirements.txt"])
+            .with_exec(["pytest"])
+        )
+
+        await container.exit_code()
+
+dagger.run(main)
